87c5718bbb38e24de4bcdd5dbf24b4ad6dcaa626
nnaeyno <ngogo20@freeuni.edu.ge>
2024-10-10 00:14:20 +0400

    models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    category_id = models.IntegerField()
+    parent = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        related_name="subcategories",
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.title
+
+    def get_main_category(self):
+        if self.parent:
+            return f"{self.parent.get_main_category()} -> {self.title}"
+        return self.title
+
+    def get_direct_category(self):
+        if self.parent:
+            return self.parent.title
+        return self.title
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    categories = models.ManyToManyField(Category)
+    product_id = models.IntegerField()
+    product_image = models.ImageField(upload_to="products/")
+
+    def __str__(self):
+        return self.name
